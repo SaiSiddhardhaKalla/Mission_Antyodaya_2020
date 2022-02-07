4ff3b47f4d135900dbecd915e23953caadce3377
SaiSiddhardhaKalla <53157278+SaiSiddhardhaKalla@users.noreply.github.com>
2022-02-07 12:30:35 +0530

    Update Dash.py

--- Raj_Antyodaya/Dash.py
+++ Raj_Antyodaya/Dash.py
@@ -30,7 +30,7 @@ m = folium.Map(location=[26.5844, 73.8496],zoom_start=6, tiles='cartodbpositron'
 # df = pd.read_csv("Raj_dt_ma.csv")
 df = pd.read_csv('https://raw.githubusercontent.com/SaiSiddhardhaKalla/Mission_Antyodaya_2020/main/Raj_Antyodaya/Raj_dt_ma.csv')
 # geoData = open('Raj_dt_ma.geojson', 'r')
-geoData = open('https://raw.githubusercontent.com/SaiSiddhardhaKalla/Mission_Antyodaya_2020/main/Raj_Antyodaya/Raj_dt_ma.geojson','r')
+geoData = open('https://raw.githubusercontent.com/SaiSiddhardhaKalla/Mission_Antyodaya_2020/main/Raj_Antyodaya/Raj_dt_ma.geojson')
 
 # Reading ac data
 geo_ac_data = gpd.read_file("Raj_AC_ma.geojson")
