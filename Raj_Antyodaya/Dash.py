970096c6ec785f3a3d71e47fc96fd7f188b9ee19
SaiSiddhardhaKalla <53157278+SaiSiddhardhaKalla@users.noreply.github.com>
2022-02-07 12:41:08 +0530

    Update Dash.py

--- Raj_Antyodaya/Dash.py
+++ Raj_Antyodaya/Dash.py
@@ -83,8 +83,8 @@ def calculate_opacity(df, category, i):
 if district == "All":
 
     choropleth = folium.Choropleth(
-#         geo_data=json.load(geoData),
-        geo_data=json.dump(geoData),
+        geo_data=json.load(geoData),
+#         geo_data=json.dump(geoData),
         name='choropleth',
         data=df,
         columns=['District Name', str(category)],
